4251ee52ad711050679750f213ea9f178de4d2ae
IlhamHFZ <ilhamhafidzfahry@gmail.com>
2024-11-25 09:17:07 +0700

    add person class

---
+++ remote-repo-to-local-repo/Person.cs
@@ -0,0 +1,9 @@
+﻿namespace remote_repo_to_local_repo
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Author { get; set; }
+}
